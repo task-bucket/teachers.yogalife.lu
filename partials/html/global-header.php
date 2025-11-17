c391fed5d3c9ce91e5aed16fb25076bba068393c
Anurag Harsh <anurag@taskbucket.com>
2025-11-17 16:10:38 +0530

    changes

--- partials/html/global-header.php
+++ partials/html/global-header.php
@@ -3,7 +3,7 @@
         <div class='col col-xs-10 col-lg-5'>
             <div class='logo-container'>
                 <div class='logo'>
-                    <a href='<?php echo $site_url; ?>'><img src='<?php echo $cdn_url; ?>/media/logo/yogalife-logo.webp ?>' alt='Yogalife logo'></a>
+                    <a href='<?php echo $site_url; ?>'><img src='<?php echo $cdn_url; ?>/media/logo/yogalife-logo.webp' alt='Yogalife logo'></a>
                 </div>
             </div>
         </div>
