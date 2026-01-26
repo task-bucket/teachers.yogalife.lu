cf31bda86daccd1a46d8972c8cbaf030c95557fc
hemant gaba <hemantg@taskbucket.com>
2026-01-26 19:26:39 +0530

    changes

--- pages/paid-listings/manoj-bhanot.php
+++ pages/paid-listings/manoj-bhanot.php
@@ -2,11 +2,10 @@
 <html lang="en">
 <head>
   <link rel="icon" type="image/x-icon" href="<?php echo $cdn_url; ?>/favicon.ico">  
-  <title>Yogalife Yoga Teachers in Luxembourg – Certified Instructor Directory</title>
+  <title>Yogalife Yoga Teachers in Luxembourg – Manoj Bhanot</title>
   <meta name="description" content="Browse the complete directory of certified Yogalife yoga teachers in Luxembourg. View profiles, experience, specialties, training background, and teaching styles. Connect with trusted, experienced yoga instructors near you." />
   <?php require_once('partials/html/global-header-includes.php') ?>
   <meta charset="UTF-8">
-  <title>Manoj Bhanot - Yogalife</title>
   <link rel="stylesheet" href="style.css">
   <style>
       <?php require_once('partials/css/spark-grid.php') ?>
